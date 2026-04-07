ea3adc396320af63c1488835986626b8baf6f2ac
Ahmadjon-Ergashev <111.ahmadjon@gmail.com>
2026-04-07 12:40:09 +0500

    fix: database

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
-class ApiController extends Controller
+class ApiController
 {
     // Admin Login
     public function login(Request $request) {
